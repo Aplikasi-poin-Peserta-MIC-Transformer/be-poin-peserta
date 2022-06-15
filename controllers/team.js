73d1880b7b9b6ce2a295efb60c0a28c38af72c6e
tomyoktavian <drivehartomy@gmail.com>
2022-06-16 00:43:08 +0700

    set poin and pos if null

--- controllers/team.js
+++ controllers/team.js
@@ -128,8 +128,10 @@ class TeamController {
       });
       const event = await Event.findOne({ where: { id: team.EventId } })
       const [total_poin, metadata] = await sequelize.query(`select ifnull(total_poin,0) as point from Points where TeamId_or_UserId = ${id} limit 0,1`)
+      const point = total_poin.legnth > 0 ? total_poin[0].point : 0
       const [pos, posMeta] = await sequelize.query(`select pos from Pos_steps where TeamId_or_UserId = ${id} limit 0,1`)
-      res.status(200).json({ ...team.dataValues, nama_event: event.nama_event, total_poin: total_poin[0]?.point, pos: pos[0]?.pos, jml_pos: event.jml_pos });
+      const poCount = pos.legnth > 0 ? pos[0].pos : 0
+      res.status(200).json({ ...team.dataValues, nama_event: event.nama_event, total_poin: point, pos: poCount, jml_pos: event.jml_pos });
     }
     catch (err) {
       next(err);
